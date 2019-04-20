0b2cfba5964cd068aebc928e005c61c423b6260f
Aryn Davis <arynlynn1@gmail.com>
2019-04-20 16:43:05 -0500

    new component for user programs

---
+++ src/app/user-programs/user-programs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-programs',
+  templateUrl: './user-programs.component.html',
+  styleUrls: ['./user-programs.component.css']
+})
+export class UserProgramsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
