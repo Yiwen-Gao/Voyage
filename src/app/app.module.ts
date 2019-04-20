55cdc3653361ba265a4a9f9907159249cdbe2408
suxiao li <suxiaoli@suxiaos-MacBook-Pro.local>
2019-04-20 13:16:55 -0400

    initialing skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ProgramFinderComponent } from './program-finder/program-finder.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ProgramTrackerComponent } from './program-tracker/program-tracker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgramFinderComponent,
+    UserProfileComponent,
+    ProgramTrackerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProgramFinderComponent },
+      { path: 'user-profile/:username', component: UserProfileComponent },
+      { path: 'program-tracker/:username', component: ProgramTrackerComponent }
+    ]),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/program-finder/program-finder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-program-finder',
+  templateUrl: './program-finder.component.html',
+  styleUrls: ['./program-finder.component.css']
+})
+export class ProgramFinderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
