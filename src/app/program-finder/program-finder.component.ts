f0cdc2f577ac0c454e5cda16639794224df63596
suxiao li <suxiaoli@suxiaos-MacBook-Pro.local>
2019-04-20 19:05:08 -0400

    add import

--- src/app/program-finder/program-finder.component.ts
+++ src/app/program-finder/program-finder.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {ENTER, COMMA} from '@angular/cdk/keycodes';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 const PROGRAM_DATA = [
-  {program: 'CS Masters', university: 'Columbia'},
-  {program: 'CS phD', university: 'MIT'},
-  {program: 'CS MPhil', university: 'UCLA'}
+  { program: 'CS Masters', university: 'Columbia' },
+  { program: 'CS phD', university: 'MIT' },
+  { program: 'CS MPhil', university: 'UCLA' }
 ];
 
 @Component({
@@ -13,21 +14,18 @@ const PROGRAM_DATA = [
   styleUrls: ['./program-finder.component.css']
 })
 export class ProgramFinderComponent implements OnInit {
-
   colNames = ['program', 'university'];
   dataSource = PROGRAM_DATA;
   filters: string[] = [];
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   add(event: MatChipInputEvent): void {
     this.filters.push(event.value);
-    if (event.input)
-      event.input.value = '';
+    if (event.input) { event.input.value = ''; }
   }
 
   remove(filter: string): void {
