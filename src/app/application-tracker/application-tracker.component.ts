cdda79af3a0f6229d944b01238e0b3592bf898e3
suxiao li <lisuxiaoserena@gmail.com>
2019-04-20 15:44:45 -0400

    adding bootstrap example

--- src/app/application-tracker/application-tracker.component.ts
+++ src/app/application-tracker/application-tracker.component.ts
@@ -1,6 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+interface Country {
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
+
+const COUNTRIES: Country[] = [
+  {
+    name: 'Russia',
+    flag: 'f/f3/Flag_of_Russia.svg',
+    area: 17075200,
+    population: 146989754
+  },
+  {
+    name: 'Canada',
+    flag: 'c/cf/Flag_of_Canada.svg',
+    area: 9976140,
+    population: 36624199
+  },
+  {
+    name: 'United States',
+    flag: 'a/a4/Flag_of_the_United_States.svg',
+    area: 9629091,
+    population: 324459463
+  },
+  {
+    name: 'China',
+    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+    area: 9596960,
+    population: 1409517397
+  }
+];
+
 @Component({
   selector: 'app-application-tracker',
   templateUrl: './application-tracker.component.html',
@@ -10,6 +44,8 @@ export class ApplicatoinTrackerComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   username: string;
+
+  countries = COUNTRIES;
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.username = params.get('username');
