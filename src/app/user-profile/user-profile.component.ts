0c0ce43e6888e55b2675be3872fa1b3379ce0952
suxiao li <suxiaoli@suxiaos-MacBook-Pro.local>
2019-04-21 15:44:16 -0400

    removed uncecessary files

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProfileFormModel } from '../form-model/form-model';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,15 +7,6 @@ import { ProfileFormModel } from '../form-model/form-model';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  profileModel = new ProfileFormModel(
-    'Serena',
-    'Li',
-    'Female',
-    '111 sample drive',
-    '123-456-7890',
-    'serena.li@columbia.edu'
-  );
-
   genders = ['Male', 'Female'];
   ngOnInit() {}
 }
